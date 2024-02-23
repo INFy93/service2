fa5687fe63f2b5a5e91a1d03de1b4b2603f79eb0
INFy93 <infy93@gmail.com>
2024-02-23 10:11:26 +0300

    make UI for services resource

--- app/MoonShine/Resources/ServiceResource.php
+++ app/MoonShine/Resources/ServiceResource.php
@@ -7,6 +7,7 @@ namespace App\MoonShine\Resources;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Service;
 
+use MoonShine\Fields\Text;
 use MoonShine\Resources\ModelResource;
 use MoonShine\Decorations\Block;
 use MoonShine\Fields\ID;
@@ -20,11 +21,22 @@ class ServiceResource extends ModelResource
 
     protected string $title = 'Сервисы';
 
+    protected string $sortDirection = 'ASC';
+
+    protected bool $createInModal = true;
+
+    protected bool $editInModal = true;
+
+    protected bool $detailInModal = true;
+
     public function fields(): array
     {
         return [
             Block::make([
                 ID::make()->sortable(),
+                Text::make('Сервис', 'service_name'),
+                Text::make('Буквенный код', 'service_code'),
+                Text::make('Адрес', 'address'),
             ]),
         ];
     }
